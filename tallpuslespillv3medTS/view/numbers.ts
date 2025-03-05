40f63ed06e62f7653b96935f7d7f7ef5d0196a54
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-05 13:38:32 +0100

    counter types

--- tallpuslespillv3medTS/view/numbers.ts
+++ tallpuslespillv3medTS/view/numbers.ts
@@ -5,7 +5,7 @@ const getNumberElement = (number: number, index : number) => {
   return `<div onclick="clickedNumber(${index})" id="square">${number ?? ''}</div>`;
 };
 
-export default (targetElement : HTMLElement , { numbers  }) => {
+export default (targetElement: { cloneNode: (arg0: boolean) => any; }  , { numbers  }: any) => {
   const newNumbersList = targetElement.cloneNode(true);
   const numbersElements = numbers.map(getNumberElement).join("");
   newNumbersList.innerHTML = numbersElements;
