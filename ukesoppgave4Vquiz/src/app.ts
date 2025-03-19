e8fabf2d8fbc2bf77359fa92c8650786ff599b80
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-19 14:03:54 +0100

    testing shadowroot

---
+++ ukesoppgave4Vquiz/src/app.ts
@@ -0,0 +1,4 @@
+import FromToComponent  from "./components/fromTo-component";
+
+customElements.define("fromTo-container", FromToComponent);
+
---
+++ ukesoppgave4Vquiz/src/components/fromTo-component.ts
@@ -0,0 +1,36 @@
+export default class FromToComponent extends HTMLElement {
+    private year = "2024";
+    constructor() {
+      super();
+      this.attachShadow({ mode: "open"})
+    }
+    connectedCallback() {
+      this.render();
+      this.setUpListeners();
+    }
+  
+    private render() {
+      this.shadowRoot!.innerHTML = /*HTML*/ `
+      <dato-component>
+      <div
+          style="
+            display: inline-flex;
+            flex-direction: row;
+            align-items: center;
+            column-gap: 6px;
+          ">
+          <div style="font-size: 300%; margin-right: 0px">${this.year}</div>
+          <div style="display: flex; flex-direction: column">
+            <button id="year-up">▲</button>
+            <button data-year-down>▼</button>
+          </div>
+        </div>
+        </dato-component>
+        `;
+    }
+  
+    private setUpListeners() {
+      const btnYearUp = document.querySelector("#year-up");
+      btnYearUp?.addEventListener("click", () => console.log('Click'));
+    }
+  }
