e9e1729347ad2021124cb028e934c9e9b6587767
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-20 14:49:04 +0100

    validate all the things

--- ukesoppgave4/src/components/Validate-date.ts
+++ ukesoppgave4/src/components/Validate-date.ts
@@ -3,19 +3,13 @@ import { fromDate, toDate } from "../index";
 export default class ValidateTimeComponent extends HTMLElement {
   connectedCallback() {
     window.requestAnimationFrame(() => {
-      // const fromIsValid = fromDate.checkDateIsValid(
-      //   `${fromDate.Day} ${fromDate.Month} ${fromDate.Year}`
-      // );
-      // const toIsValid = toDate.checkDateIsValid(
-      //   `${toDate.Day} ${toDate.Month} ${toDate.Year}`
-      // );
       const div = document.createElement("div");
       const divValid = document.createElement("div");
       divValid.classList.add("validate");
       divValid.setAttribute("id", "validate");
       div.appendChild(divValid);
-      this.appendChild(div);
-      document.body.addEventListener("click", () =>
+      
+      document.body.addEventListener("click", async () =>
         fromDate.checkDateIsValid(
           `${fromDate.Day} ${fromDate.Month} ${fromDate.Year}`
         ) &&
@@ -23,6 +17,7 @@ export default class ValidateTimeComponent extends HTMLElement {
           ? (divValid.textContent = `Date is valid`)
           : (divValid.textContent = `Date is NOT valid!!!!`)
       );
+      this.appendChild(div);
     });
   }
 }
