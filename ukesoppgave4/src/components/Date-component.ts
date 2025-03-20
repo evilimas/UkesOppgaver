88a679dc9fe91685f8e6920b6580ef6365483a8f
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-20 11:28:04 +0100

    testing some OO stuff

--- ukesoppgave4/src/components/Date-component.ts
+++ ukesoppgave4/src/components/Date-component.ts
@@ -1,10 +1,22 @@
-interface DateData {
-    day: string
-    month: string
-    year:string
-    date: string
-}
+export class DateComponent {
+    private Day: number
+    private Month: number
+    private Year: number
+    private DateString: string
+    
+    public constructor(day: number, month: number, year: number){
+        this.Day = day
+        this.Month = month
+        this.Year = year
+        this.DateString = ""
+    }
 
-class DateChecker implements DateData{
-    day: string
+    public makeDateString(day: number,month: number,year: number): string {
+        const date = `${day.toString()}-${month.toString()}-${year.toString()}`
+        return date
+    }
+    public checkDateIsValid(DateString: string){
+        const date = new Date(DateString)
+        return !isNaN(date.getTime())
+    }
 }
