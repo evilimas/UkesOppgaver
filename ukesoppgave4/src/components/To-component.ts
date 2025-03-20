fc563531d032cc90fb502cdd5e494cd1d399a163
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-20 13:33:52 +0100

    testing some OO stuff

---
+++ ukesoppgave4/src/components/To-component.ts
@@ -0,0 +1,48 @@
+// import { month } from "./Month-component";
+import { toDate as date } from "../index"
+
+
+export default class ToTimeComponent extends HTMLElement {
+  connectedCallback() {
+    window.requestAnimationFrame(() => {
+      const div = document.createElement("div");
+
+      const divDate = document.createElement("div");
+      divDate.classList.add("day");
+      divDate.setAttribute("id", "from-date");
+      divDate.textContent = `${date.Day} ${date.Month} ${date.Year}`;
+
+      const divBtns = document.createElement("div");
+      divBtns.classList.add("day1");
+
+      const btnUp = document.createElement("button");
+      btnUp.classList.add("day-up");
+      btnUp.textContent = "▲";
+
+      const btnDown = document.createElement("button");
+      btnDown.classList.add("day-down");
+      btnDown.textContent = "▼";
+
+      divBtns.appendChild(btnUp);
+      divBtns.appendChild(btnDown);
+      div.appendChild(divDate);
+      div.appendChild(divBtns);
+
+      this.appendChild(div);
+      const dayUpButton = div.querySelector(".day-up");
+      const dayDownButton = div.querySelector(".day-down");
+      dayUpButton?.addEventListener("click", () => {
+        date.Day++
+        divDate.textContent = `${date.Day} ${date.Month} ${date.Year}`;
+        if (date.Day >= 31){
+          date.Day = 0
+        }
+      });
+
+      dayDownButton?.addEventListener("click", () => {
+        date.Day--
+        divDate.textContent = `${date.Day} ${date.Month} ${date.Year}`;
+      });
+    });
+  }
+}
