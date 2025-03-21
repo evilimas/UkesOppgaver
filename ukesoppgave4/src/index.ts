44aa703dfb2dc42468643c2575a7ceb563449943
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-21 13:21:06 +0100

    styling

--- ukesoppgave4/src/index.ts
+++ ukesoppgave4/src/index.ts
@@ -1,11 +1,3 @@
-// import YearComponent from './components/Year-component';
-// import MonthComponent from './components/Month-component';
-// import DayComponent from './components/Day-component';
-
-
-// window.customElements.define('dato-from-component', YearComponent);
-// window.customElements.define('month-component', MonthComponent);
-// window.customElements.define('day-component', DayComponent);
 
 import { DateComponent } from "./components/Date-component";
 import FromTimeComponent from "./components/From-component";
