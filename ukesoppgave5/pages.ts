0192815dca6fa36ab99fe3373fa306bc98b7783e
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-03-27 13:10:24 +0100

    x

--- ukesoppgave5/pages.ts
+++ ukesoppgave5/pages.ts
@@ -6,9 +6,11 @@ export default (container) => {
   };
 
   const list = () => {
+    let html = '';
     for (let movie of model.movies) {
-      container.innerHTML += /*HTML*/ `
-            <div class="movie" onclick="showMovie(${movie.id})">
+      html += /*HTML*/ `
+      <a href="/list/${movie.id}">
+            <div class="movie">
                 <div>
                     <img src="${movie.imageUrl}" style="height: 120px"/>
                 </div>
@@ -17,13 +19,31 @@ export default (container) => {
                     ${movie.year}<br/>
                     ${movie.genre}
                 </div>
-            </div>`;
+            </div>
+        </a>`;
     }
+    container.innerHTML = html;
   };
 
   const detail = (params) => {
     const { id } = params;
-    container.textContent = `This is Detail Page with Id ${id}`;
+    let movieId = model.movies.find((x) => x.id == id);
+    container.innerHTML = /*HTML*/ `
+    <h1>${movieId?.title}</h1>
+    <div>
+        <img src="${movieId?.imageUrl}" style="height: 120px"/>
+    </div>
+    <div>
+        <b></b><br/>
+        ${movieId?.year}<br/>
+        ${movieId?.genre}
+    </div>
+    <h3>Rating</h3>
+    <h3>Gi din rating: </h3>
+    <div style="display: flex; font-size: 200%">
+      
+    </div>
+`;
   };
 
   const notFound = () => {
