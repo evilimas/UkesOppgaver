041181291b3ea91e9523cb761597949678d65b69
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-31 13:35:45 +0200

    case get studentadmin

---
+++ casev0.01/src/index.ts
@@ -0,0 +1,15 @@
+import createRouter from './router.ts';
+import createPages from './render.ts';
+
+const container: HTMLElement | null =
+  document.querySelector<HTMLElement>('#app');
+
+const pages = createPages(container!);
+
+const router = createRouter();
+
+router.addRoute('/', pages.home);
+    .addRoute('/list', pages.list);
+    .addRoute('/list/:id', pages.detail);
+    .setNotFound(pages.notFound);
+    .start();
---
+++ casev0.01/src/render.ts
@@ -0,0 +1,56 @@
+import model from './model/model';
+
+export default (container: HTMLElement) => {
+  const home = () => {
+    container.innerHTML = 'This is Home page - go to student list';
+  };
+
+  const list = () => {
+    let html = '';
+    for (let student of model.students) {
+      html += /*HTML*/ `
+      <a href="/list/${student.id}">
+            <div class="student">
+                <div>
+                    <b>${movie.title}</b><br/>
+                    ${movie.year}<br/>
+                    ${movie.genre}
+                </div>
+            </div>
+        </a>`;
+    }
+    container.innerHTML = html;
+  };
+
+  const detail = (params) => {
+    const { id } = params;
+    let movieId = model.movies.find((x) => x.id == id);
+    container.innerHTML = /*HTML*/ `
+    <h1>${movieId?.title}</h1>
+    <div>
+        <img src="${movieId?.imageUrl}" style="height: 120px"/>
+    </div>
+    <div>
+        <b></b><br/>
+        ${movieId?.year}<br/>
+        ${movieId?.genre}
+    </div>
+    <h3>Rating</h3>
+    <h3>Gi din rating: </h3>
+    <div style="display: flex; font-size: 200%">
+      
+    </div>
+`;
+  };
+
+  const notFound = () => {
+    container.innerHTML = 'Page Not Found!';
+  };
+
+  return {
+    home,
+    list,
+    detail,
+    notFound,
+  };
+};
