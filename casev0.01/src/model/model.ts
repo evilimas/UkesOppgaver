a2a8c2852ff8ad146b4c74d2e1c955523cf96caa
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-03-31 13:50:19 +0200

    tester model

---
+++ casev0.01/src/model/model.ts
@@ -0,0 +1,18 @@
+
+export default {
+    app:{
+
+    },
+    students: {
+        id: 1,
+        name: "Per Spelmann",
+        emailPriv: "per@mail.no",
+        emailGet: "per.spelmann@getacademy.no",
+        kurs: "FrontEnd",
+        kursType: "Fagskole",
+        startDato: "2025-08-12",
+        sluttdato: null,
+        telefonNummer: "000 00 000",
+        discordNavn: "per.spelmann",
+    },
+}
