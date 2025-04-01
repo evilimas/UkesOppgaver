fa221cfb90c815a6ff5480f74636c9960bc121df
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-04-01 11:03:03 +0200

    x

--- casev0.01/src/components/candidate-component.ts
+++ casev0.01/src/components/candidate-component.ts
@@ -2,7 +2,7 @@ import type { Candidate } from '../model/types';
 
 export default class Student extends HTMLElement {
   connectedCallback() {
-    const candidateJson: string = this.getAttribute('movie') ?? '{}';
+    const candidateJson: string = this.getAttribute('candidate') ?? '{}';
     const candidate: Candidate = JSON.parse(candidateJson);
     window.requestAnimationFrame(() => {
       this.innerHTML = /*HTML*/ `
