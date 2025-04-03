946655bf7e8076e0a6dc9a1e5ba619d5e81680a5
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-04-03 14:02:58 +0200

    v0.02 rewriting types

--- casev0.02/src/components/candidate-component.ts
+++ casev0.02/src/components/candidate-component.ts
@@ -1,9 +1,9 @@
-import type { ICandidate } from '../model/types';
+import type { Candidate } from '../model/types';
 
 export default class Student extends HTMLElement {
   connectedCallback() {
     const candidateJson: string = this.getAttribute('candidate') ?? '{}';
-    const candidate: ICandidate = JSON.parse(candidateJson);
+    const candidate: Candidate = JSON.parse(candidateJson);
     window.requestAnimationFrame(() => {
       this.innerHTML = /*HTML*/ `
       
