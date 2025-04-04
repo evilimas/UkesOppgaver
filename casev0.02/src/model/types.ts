00647e773c818ce5c5817bf2f74b12a4777ab4ac
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-04-04 09:13:37 +0200

    v0.02 rewriting types

--- casev0.02/src/model/types.ts
+++ casev0.02/src/model/types.ts
@@ -6,45 +6,44 @@ export type Candidate = {
   phoneNumber?: string;
   discordName?: string;
   profilPicture?: string;
-}
+};
 
 export type Course = {
   id: number;
   name: string;
-  startDate: string
-  endDate: string
-  price: number
-  semester: Semester
-}
+  startDate: string;
+  endDate: string;
+  price: number;
+  semester: Semester;
+};
 
 export type Semester = {
-  name: string
-  fromDate: string
-  toDate: string
-}
+  name: string;
+  fromDate: string;
+  toDate: string;
+};
 export type CourseCandidate = {
-      courseId: number[]
-      candidateId: number
-      applied: boolean;
-      started: boolean;
-      startDate: Date;
-      endDate: Date;
-      semesterId: number;
-      completed: boolean;
-      quit: boolean;
-      paymentAdded: PaymentAdded
-    }
+  courseId: number[];
+  candidateId: number;
+  applied: boolean;
+  started: boolean;
+  startDate: Date;
+  endDate: Date;
+  semesterId: number;
+  completed: boolean;
+  quit: boolean;
+  paymentAdded: PaymentAdded;
+};
 
 export type PaymentAdded = {
-  payDate: Date
-  amount: number
-  balance: number
-  payedInFull: boolean
+  payDate: Date;
+  amount: number;
+  balance: number;
+  payedInFull: boolean;
 };
-  
+
 export interface AppState {
   app: any;
   candidates: Candidate[];
   courses: Course[];
 }
-
