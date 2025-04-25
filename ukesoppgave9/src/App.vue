072ca59baac3384e8f8950ef76a60f79eef82589
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-04-25 11:04:40 +0200

    init

---
+++ ukesoppgave9/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SlidingPuzzle from './components/SlidingPuzzle.vue'
+</script>
+
+<template>
+
+  <SlidingPuzzle msg="Sliding puzzle" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ ukesoppgave9/src/components/SlidingPuzzle.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+
+
+const numbers = ref<(number | null)[]>([3, 5, 6, 8, 1, null, 2, 4, 7])
+// const numbers = ref({
+//   0: 3,
+//   1: 5,
+//   2: 6,
+//   3: 8,
+//   4: 1,
+//   5: null,
+//   6: 2,
+//   7: 4,
+//   8: 7 
+// })
+
+const count = ref<number>(0)
+
+
+function clickedNumber(index : number) : void {
+    count.value++;
+    let blankIndex = findBlankIndex(index);
+    if (blankIndex == null) return;
+    numbers.value[blankIndex] = numbers.value[index];
+    numbers.value[index] = null;
+   
+    
+    
+}
+
+function findBlankIndex(index : number) : number | null {
+    for (let delta of [-3, -1, 1, 3]) {
+        let newIndex = index + delta;
+        if (newIndex < 0 || newIndex >= numbers) continue;
+        if (numbers.value[newIndex] == null) {
+            return newIndex;
+        }
+    }
+    return null;
+}
+
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div v-for="(number, i) in numbers" :key="i" :class="i % 3 == 0 ? 'flis førstPåLinje' : 'flis'" @click="clickedNumber(i);" >{{number}}
+    </div>
+    <div >Antall trekk: {{count}}</div>
+  </div>
+
+
+</template>
+
+<style scoped>
+p.feilmelding {
+            color: red;
+        }
+
+        div.trekkteller {
+            clear: left;
+        }
+
+        div.flis {
+            color: blue;
+            background: lightblue;
+            padding: 18px;
+            margin: 10px;
+            border: solid green 7px;
+            width: 30px;
+            height: 30px;
+            float: left;
+        }
+
+        div.førstPåLinje {
+            clear: left;
+        }
+</style>
