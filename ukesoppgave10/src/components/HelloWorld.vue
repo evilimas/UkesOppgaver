8c50b83559595ba0ed3e7755fa4613711af70abe
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-04-30 10:13:18 +0200

    x

--- ukesoppgave10/src/components/HelloWorld.vue
+++ ukesoppgave10/src/components/HelloWorld.vue
@@ -3,8 +3,8 @@ import { ref } from 'vue'
 
 type Suit = 'Hearts' | 'Diamonds' | 'Clubs' | 'Spades'
 type Rank = 'Ace' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'Jack' | 'Queen' | 'King'
-type Card = { suit: Suit; rank: Rank; }
-
+type Card = { suit: Suit; rank: Rank; seed: number }
+// type SeededCard = { suit: Suit; rank: Rank }
 
 const makeDeck = (): Card[] => {
   const suits: Suit[] = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
@@ -23,13 +23,14 @@ const makeDeck = (): Card[] => {
     'Queen',
     'King',
   ]
-  return suits.flatMap(suit => ranks.map(rank => ({ suit, rank, })));
+  return suits.flatMap((suit) => ranks.map((rank) => ({ suit, rank })))
   // return suits.flatMap((suit) => ranks.map((rank) => seeds.map((seed) => ({ suit, rank, seed }))))
 }
 
 const deck = ref<Card[]>(makeDeck())
+const shuffledDeck = ref<Card[]>()
 const drawnCard = ref<Card | null>(null)
-
+console.log(deck)
 const generateRandomSequence = (seed: number, length: number): number[] => {
   const result: number[] = []
   let current = seed
@@ -40,15 +41,24 @@ const generateRandomSequence = (seed: number, length: number): number[] => {
   return result
 }
 const shuffleDeck = (deck: Card[]) => {
-  console.log(generateRandomSequence(Math.floor(Math.random() * 1000000), deck.length))
+  let seededDeck = []
+  const seeds = generateRandomSequence(Math.floor(Math.random() * 1000000), deck.length)
+  for (let card of deck) {
+    for (let seed of seeds) {
+      seededDeck.push(Object.assign(card, { seed }))
+    }
+  }
+  console.log(seededDeck)
+
+  // console.log(generateRandomSequence(Math.floor(Math.random() * 1000000), deck.length))
   // map random to deck
   // sort deck
   // return shuffled deck
 }
+
 const drawCard = (deck: Card[]): { remainingDeck: Card[]; card?: Card } => {
   if (deck.length === 0) return { remainingDeck: [], card: undefined }
   const [first, ...rest] = deck
-  console.log(deck)
   shuffleDeck(deck)
   return { remainingDeck: rest, card: first }
 }
