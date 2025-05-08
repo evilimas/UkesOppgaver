0f399ae737c1069cc5e2f8b9e2421e189023da9d
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-05-08 14:04:55 +0200

    x

--- ukesoppgave11/src/components/Yatzy.vue
+++ ukesoppgave11/src/components/Yatzy.vue
@@ -2,19 +2,27 @@
 import { ref, computed } from 'vue';
 import type { Dice, rollDie } from '../gameLogic';
 
-
 const rollDie = () => Math.floor(Math.random() * 6) + 1;
 const rollDice = (n: number): Dice => Array.from({ length: n }, rollDie);
-const numberOfRolls = ref(3);
+const numberOfRolls = ref<number>(3);
 const currentDice = ref<number[]>([]);
 const NumberOfDice = ref<number>(7);
 const savedDice = ref<number[]>([]);
 const mostPoints = ref<number[]>([]);
+const selectedIndex = ref<number[]>([]);
+
+const selectedDice = computed(() => {
+  selectedIndex.value!.map((index) => currentDice.value[index]);
+});
+
 const addtosavedDice = () => {
- currentDice.value.pop()
-  savedDice.value.push(currentDice.value[0]);
+  // return currentDice.map((index) => savedDice[index]);
+  // currentDice.value.unshift();
+  // savedDice.value.push(currentDice.value[0]);
 };
+
 function updateNumberOfRolls() {
+  currentDice.value = rollDice(6);
   numberOfRolls.value--;
 }
 </script>
@@ -23,17 +31,21 @@ function updateNumberOfRolls() {
   <div class="app">
     <h1>Yatzy</h1>
     <h3>Spill Yatzy med oss!</h3>
-    <button @click="emit(updateNumberOfRolls)">Kast terninger </button>
-    <div v-if="numberOfRolls > 0"> {{  NumberOfRolls }}</div>
+    <button @click="updateNumberOfRolls" :disabled="numberOfRolls == 0">
+      Kast terninger
+    </button>
+    <div v-if="numberOfRolls > 0">{{ numberOfRolls }}</div>
     <div v-else>Du har brukt dine kast</div>
-    <p v-for="die,index of currentDice" :key=index><input type="checkbox">{{ die }}</p>
-    <button @click="addtosavedDice()">Spar terninger</button>
-    <br>
-    <button @click="checkPoints(savedDice)">Finn kombo med mest poeng</button>
-    <p> {{ mostPoints }}</p>
+    <p v-for="(die, index) of currentDice" :key="index">
+      <input type="checkbox" v-model="selectedIndex" :value="index" />{{ die }}
+    </p>
+    {{}}
+    <!-- <button @click="">Spar terninger</button> -->
+    <br />
+    <!-- <button @click="checkPoints(savedDice)">Finn kombo med mest poeng</button> -->
+    <p v-for="(die, index) of savedDice" :key="index">{{ die }}</p>
+    <p>{{ mostPoints }}</p>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
