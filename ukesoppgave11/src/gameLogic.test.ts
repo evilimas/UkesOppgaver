b710c9f864880c89765e63e1ba73143937bd2203
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-05-09 14:26:59 +0200

    making yatzee

--- ukesoppgave11/src/gameLogic.test.ts
+++ ukesoppgave11/src/gameLogic.test.ts
@@ -1,10 +1,71 @@
-import { describe, it, expect } from 'vitest';
-import { countDie, nOfAKind } from './gameLogic.ts';
+import { describe, it, expect } from 'vitest'
+import * as Y from './yatzyLogic';
+import type {Die, DieFrequencyTable} from './yatzyLogic';
 
-describe('countDie', () => {
-  it('count the times of the same die', () => {
-    const dice = [1, 2, 3, 3, 3, 4];
+describe('YatzyLogic', () => {
+  it('Frekvenstabell for terninger 1, 1, 1, 2, 3', () => {
+    // arrange
+    const dice : Die[] = [1, 1, 1, 2, 3];
 
-    expect(countDie(dice).toEqual({ 1: 1, 2: 1, 3: 3, 4: 1 }));
-  });
-});
+    // act
+    const counts : DieFrequencyTable = Y.createFrequencyTable(dice);
+
+    // assert
+    expect(counts).toEqual({1: 3, 2: 1, 3: 1, 4: 0, 5: 0, 6: 0});
+  })
+
+  it('Ikke ett par', () => {
+    // arrange
+    const dice : Die[] = [1, 2, 3, 4, 5];
+
+    // act
+    const points = Y.pointsOnePair(dice);
+
+    // assert
+    expect(points).toBe(0);
+  })
+
+  it('Par i 5', () => {
+    // arrange
+    const dice : Die[] = [1, 5, 3, 4, 5];
+
+    // act
+    const points = Y.pointsOnePair(dice);
+
+    // assert
+    expect(points).toBe(10);
+  })
+
+  it('Par i 3 og 5', () => {
+    // arrange
+    const dice : Die[] = [3, 5, 3, 4, 5];
+
+    // act
+    const points = Y.pointsOnePair(dice);
+
+    // assert
+    expect(points).toBe(10);
+  })
+
+  it('Fire like 2', () => {
+    // arrange
+    const dice : Die[] = [2, 2, 1, 2, 2];
+
+    // act
+    const points = Y.pointsFourOfAKind(dice);
+
+    // assert
+    expect(points).toBe(8);
+  })
+
+  it('Yatzy', () => {
+    // arrange
+    const dice : Die[] = [2, 2, 2, 2, 2];
+
+    // act
+    const points = Y.pointsYatzy(dice);
+
+    // assert
+    expect(points).toBe(50);
+  })
+})
