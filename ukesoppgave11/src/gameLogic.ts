05ec856f44e022183002a2a87d5773979ca8e3e1
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-05-09 10:53:40 +0200

    making yatzee

--- ukesoppgave11/src/gameLogic.ts
+++ ukesoppgave11/src/gameLogic.ts
@@ -1,42 +1,25 @@
-// This file contains the game logic for the dice game.
+type Die = number;
+type Dice = Die[];
+// type count = ()=>
 
-type Dice = number[];
-type rollDie = () => number;
-// Impure
+const countDie = (dice: Dice[]): Record<Die, number> =>
+  dice.reduce((acc, val) => {
+    acc[val] = (acc[val] || 0) + 1;
+    return acc;
+  }, {});
 
-// const rollDie = () => Math.floor(Math.random() * 6) + 1;
-// const rollDice = (n: number): Dice => Array.from({ length: n }, rollDie);
+const hasOfAKind =
+  (n: Die) =>
+  (dice: Dice[]): Boolean => {
+    const counts = Object.values(countDie(dice));
+    return counts.includes(n);
+  };
 
-//Pure
-const nOfAKind = (n: Dice) => n.filter((x) => x === n[0]).length;
-const checkPair = (n: Dice) => n.filter((x) => x === n[0]).length === 2;
-const checkThreeOfAKind = (n: Dice) => n.filter((x) => x === n[0]).length === 3;
-const checkFourOfAKind = (n: Dice) => n.filter((x) => x === n[0]).length === 4;
-const checkFiveOfAKind = (n: Dice) => n.filter((x) => x === n[0]).length === 5;
-const checkSixOfAKind = (n: Dice) => n.filter((x) => x === n[0]).length === 6;
-const checkTwoPair = (n: Dice) => {
-  const first = n.filter((x) => x === n[0]).length;
-  const second = n.filter((x) => x === n[1]).length;
-  const third = n.filter((x) => x === n[2]).length;
-  return (
-    (first === 2 && second === 2) ||
-    (first === 2 && third === 2) ||
-    (second === 2 && third === 2)
-  );
+const hasPair = hasOfAKind(2);
+const hasThreeOfAKind = hasOfAKind(3);
+const hasFourOfAKind = hasOfAKind(4);
+
+const hasTwoPairs = (dice: Dice[]) => {
+  const counts = Object.values(countDie(dice));
+  return counts.filter((n) => n === 2).length === 2;
 };
-const checkYatzy = (n: Dice) => n.filter((x) => x === n[0]).length === 6;
-const checkFullHouse = (n: Dice) => {
-  const first = n.filter((x) => x === n[0]).length;
-  const second = n.filter((x) => x === n[1]).length;
-  return (first === 2 && second === 3) || (first === 3 && second === 2);
-};
-const checkSmallStraight = (n: Dice) => {};
-const checkLargeStraight = (n: Dice) => {};
-const checkChance = (n: Dice) => n.reduce((a, b) => a + b, 0);
-
-const checkPoints = (n: Dice) => {
-  n.reduce((a, b) => a + b, 0);
-};
-
-export { nOfAKind, checkPoints };
-export type { rollDie };
