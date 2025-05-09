dc6740e22dc82a2d0a4736b61123f9db263432af
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-05-09 09:02:50 +0200

    making yatzee

--- ukesoppgave11/src/components/Yatzy.vue
+++ ukesoppgave11/src/components/Yatzy.vue
@@ -1,28 +1,34 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
-import type { Dice, rollDie } from '../gameLogic';
+import type { Dice, rollDie, findCombos } from '../gameLogic';
 
 const rollDie = () => Math.floor(Math.random() * 6) + 1;
 const rollDice = (n: number): Dice => Array.from({ length: n }, rollDie);
 const numberOfRolls = ref<number>(3);
 const currentDice = ref<number[]>([]);
-const NumberOfDice = ref<number>(7);
+const NumberOfDice = ref<number>(5);
 const savedDice = ref<number[]>([]);
 const mostPoints = ref<number[]>([]);
 const selectedIndex = ref<number[]>([]);
-
+const points = ref<number>(0);
+const checkPoints = () => savedDice.reduce((a, b) => a + b, 0)
 const selectedDice = computed(() => {
   selectedIndex.value!.map((index) => savedDice.value[index]);
 });
 
-const addtosavedDice = () => {
+const addtosavedDice = (die) => {
+  savedDice.value.push(die)
+  // currentDice.value = currentDice.value.filter((item) => item !== die);
   // return currentDice.map((index) => savedDice[index]);
   // currentDice.value.unshift();
   // savedDice.value.push(currentDice.value[0]);
 };
-
+const removeFromCurrentDice = (index) => {
+  currentDice.value = currentDice.value.splice(1, index);
+  NumberOfDice.value = currentDice.value.length;
+};
 function updateNumberOfRolls() {
-  currentDice.value = rollDice(6);
+  currentDice.value = rollDice(NumberOfDice.value);
   numberOfRolls.value--;
 }
 </script>
@@ -37,14 +43,15 @@ function updateNumberOfRolls() {
     <div v-if="numberOfRolls > 0">{{ numberOfRolls }}</div>
     <div v-else>Du har brukt dine kast</div>
     <p v-for="(die, index) of currentDice" :key="index">
-      <input type="checkbox" v-model="selectedIndex" :value="index" />{{ die }}
+      <input type="checkbox" @click="addtosavedDice(die),removeFromCurrentDice(die)"/>{{ die }}
     </p>
-    {{ savedDice }}
+    <span>{{ savedDice }}</span>
+    <span>{{ currentDice }}</span>
     <!-- <button @click="">Spar terninger</button> -->
     <br />
-    <!-- <button @click="checkPoints(savedDice)">Finn kombo med mest poeng</button> -->
+    <button @click="findCombos()">Finn kombo med mest poeng</button> 
     <p v-for="(die, index) of savedDice" :key="index">{{ die }}</p>
-    <p>{{ mostPoints }}</p>
+    <p>{{ points.value }}</p> 
   </div>
 </template>
 
