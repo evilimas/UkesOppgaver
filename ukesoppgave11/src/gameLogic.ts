1dfbb27100796367db5947657c164e4b08648f82
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-05-09 11:36:19 +0200

    x

--- ukesoppgave11/src/gameLogic.ts
+++ ukesoppgave11/src/gameLogic.ts
@@ -1,11 +1,33 @@
 type Die = number;
 type Dice = Die[];
 type rollDie = () => number;
-// type count = ()=>
+type FindCombos = (dice: Dice[]) => yatzyCombos;
+type hasPair = () => number[];
+const yatzyCombos = [
+  'ones',
+  'twos',
+  'threes',
+  'fours',
+  'fives',
+  'sixes',
+  'pair',
+  'twoPairs',
+  'threeOfAKind',
+  'fourOfAKind',
+  'fullHouse',
+  'smallStraight',
+  'largeStraight',
+  'yatzy',
+  'chance',
+] as const;
+
+type yatzyCombinations = (typeof yatzyCombos)[number];
+
+export const findCombos: FindCombos = (dice: Dice[]): yatzyCombos => {
+  return 'yatzy';
+};
 
 export const countDie = (dice: Dice[]): Record<Die, number> =>
-const findCombos = (dice: Dice[]) => {}
-const countDie = (dice: Dice[]): Record<Die, number> =>
   dice.reduce((acc, val) => {
     acc[val] = (acc[val] || 0) + 1;
     return acc;
@@ -21,9 +43,9 @@ export const nOfAKind =
 const hasPair = nOfAKind(2);
 const hasThreeOfAKind = nOfAKind(3);
 const hasFourOfAKind = nOfAKind(4);
+const hasYatzy = nOfAKind(5);
 
 const hasTwoPairs = (dice: Dice[]) => {
   const counts = Object.values(countDie(dice));
   return counts.filter((n) => n === 2).length === 2;
 };
-export { findCombos }
