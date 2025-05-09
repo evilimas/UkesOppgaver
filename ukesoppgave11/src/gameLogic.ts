d49a88ca37ee3aa9bd6a275625821342ef9b64bf
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-05-09 11:03:58 +0200

    making yatzee

--- ukesoppgave11/src/gameLogic.ts
+++ ukesoppgave11/src/gameLogic.ts
@@ -2,13 +2,10 @@ type Die = number;
 type Dice = Die[];
 type rollDie = () => number;
 // type count = ()=>
-<<<<<<< HEAD
 
 export const countDie = (dice: Dice[]): Record<Die, number> =>
-=======
 const findCombos = (dice: Dice[]) => {}
 const countDie = (dice: Dice[]): Record<Die, number> =>
->>>>>>> 40604e2ec62454aa0415b4303ddf4773a70634ce
   dice.reduce((acc, val) => {
     acc[val] = (acc[val] || 0) + 1;
     return acc;
