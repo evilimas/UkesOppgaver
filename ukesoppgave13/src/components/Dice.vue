dc536a7d13c54eac5df3d26ac0efd25b871af2df
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-05-19 12:58:19 +0200

    x

--- ukesoppgave13/src/components/Dice.vue
+++ ukesoppgave13/src/components/Dice.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import type { Die } from "../yatzyLogic";
+import { ref } from 'vue';
+import type { Die } from '../yatzyLogic';
 
 // defineProps<{ msg: string }>()
 
 const emit = defineEmits<{
-  (e: 'diceRolled', value: Die[]):void;
+  (e: 'diceRolled', value: Die[]): void;
 }>();
 
 const dice = ref<Die[]>([1, 2, 3, 4, 5]);
-const diceChars = " ⚀⚁⚂⚃⚄⚅";
+const diceChars = ' ⚀⚁⚂⚃⚄⚅';
 const roll = () => {
   for (let i = 0; i < 5; i++) {
     dice.value[i] = Math.ceil(Math.random() * 6) as Die;
   }
-  emit("diceRolled", dice.value);
+  emit('diceRolled', dice.value);
 };
 </script>
 
@@ -23,7 +23,7 @@ const roll = () => {
     <legend>Terninger</legend>
     <button @click="roll">Trill</button>
     <div>
-      <span v-for="(dieValue: Die, index: number) of dice" :key="index">
+      <span v-for="(dieValue, index) of dice" :key="index">
         {{ diceChars[dieValue] }}
       </span>
     </div>
