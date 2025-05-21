c708848afab05e145212ff57b03f472cb2e22663
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-05-21 09:46:48 +0200

    making yathzee

--- ukesoppgave13/src/stores/usePlayerStore.ts
+++ ukesoppgave13/src/stores/usePlayerStore.ts
@@ -5,7 +5,7 @@ export const usePlayerStore = defineStore('Players', ()=> {
     const playerName = ref<string>('');
     const playerNumber = ref<number>(0);
     const players = ref<{ playerName: string; playerNumber: number }[]>([]);
-
+    
     const addPlayer = (name: string, number: number) => {
         players.value.push({ playerName: name, playerNumber: number });
     };
