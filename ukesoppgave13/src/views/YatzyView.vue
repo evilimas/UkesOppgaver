b10f56f4463eef0164968a03b5570a17d8322637
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-05-21 09:36:52 +0200

    making yathzee

--- ukesoppgave13/src/views/YatzyView.vue
+++ ukesoppgave13/src/views/YatzyView.vue
@@ -1,4 +1,3 @@
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import Scoreboard from '@/components/Scoreboard.vue';
@@ -11,13 +10,16 @@ const playersNr = ref($store.playerNumber);
 </script>
 
 <template>
-  <div id="game" v-for="(index) in playersNr" :key="index">
-    <h1>Yatzy</h1>
-    <Player></Player>
-     <Dice />
-    <div>
-      <Scoreboard/>
+  <div id="game" >
+    <div v-for="(index) in playersNr" :key="index">
+      <h1>Yatzy</h1>
+      <Player></Player>
+      <Dice />
+      <div>
+        <Scoreboard />
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -29,7 +31,10 @@ const playersNr = ref($store.playerNumber);
     align-items: center;
   }
 }
+
+#game {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
 </style>
-
-
-
