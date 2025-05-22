f0995cb2845dac271e5604eefdf6b102b0f675e5
GET Academy <terje@kolderup.net>
2025-05-22 10:11:49 +0200

    Update yatzyStore.ts

--- ukesoppgave13/src/stores/yatzyStore.ts
+++ ukesoppgave13/src/stores/yatzyStore.ts
@@ -5,12 +5,19 @@ import { scoreFunctions, scoreboardFunctions, emptyScoreboard } from '../yatzyLo
 import type { Die, DieFrequencyTable, YatzyCombination, Scoreboard } from '../yatzyLogic';
 
 export const yatzyStore = defineStore('scoreBoard', () => {
+
+    // const turn = ...
+    // ideelt sett skal komponentene kun lese de reaktive variablene
+    // - og bruke dem til rendering
+    // - og at endring skjer via actions
+    const nextTurn = () => { };
     const players = ref(3);
     const scoreBoardsRaw: Scoreboard[] =
         Array.from({ length: players.value }, emptyScoreboard);
     const scoreBoards = reactive(scoreBoardsRaw);
 
     return {
+        nextTurn,
         scoreBoards
     }
 });
