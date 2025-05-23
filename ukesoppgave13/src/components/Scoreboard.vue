ddbcdbdcce5a575ff9afeb5fd9369f7c2ffbd799
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-05-23 09:36:57 +0200

    making yathzee

--- ukesoppgave13/src/components/Scoreboard.vue
+++ ukesoppgave13/src/components/Scoreboard.vue
@@ -18,7 +18,7 @@ defineProps<{
         <th
           v-for="player of store.players"
           :key="player"
-          :style="{ background: activePlayer === player ? 'lightgreen' : '' }"
+          :style="{ background: activePlayer === player ? 'darkgreen' : '' }"
         >
           Spiller: {{ player }}
         </th>
@@ -28,11 +28,16 @@ defineProps<{
       <tr v-for="(value, combination) in uiLabels" :key="combination">
         <td :id="combination">{{ value }}</td>
         <td
-          @click="store.nextTurn(combination)"
           v-for="(scoreBoard, index) of store.scoreBoards"
           :key="index"
           :id="combination"
-          
+          :class="{ disabled: scoreBoard[combination] !== null || scoreBoard[combination] == 0 }"
+          :style="{ 
+            background: activePlayer === index + 1 ? 'darkblue' : '', 
+            cursor: scoreBoard[combination] !== null ? 'not-allowed' : 'pointer',
+            opacity: scoreBoard[combination] !== null || scoreBoard[combination] == 0 ? 0.5 : 1
+          }"
+          @click="scoreBoard[combination] === null ? store.nextTurn(combination) : null"
         >
           {{ scoreBoard[combination] }}
         </td>
@@ -49,5 +54,10 @@ td {
   border: 1px solid lightblue;
   border-collapse: collapse;
   padding: 4px;
+}
+.disabled {
+  pointer-events: none;
+  background-color: #f0f0f0 !important;
+  color: #888;
 }
 </style>
