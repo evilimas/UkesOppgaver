f417d2c0473df5a8c38052927081231676b8d625
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-05-28 14:29:37 +0200

    vitest workspace

---
+++ vitest.workspace.ts
@@ -0,0 +1,9 @@
+import { defineWorkspace } from 'vitest/config'
+
+export default defineWorkspace([
+  "./ukesoppgave10/vitest.config.ts",
+  "./ukesoppgave12/vitest.config.ts",
+  "./ukesoppgave11/vitest.config.ts",
+  "./ukesoppgave14/vitest.config.ts",
+  "./ukesoppgave13/vitest.config.ts"
+])
