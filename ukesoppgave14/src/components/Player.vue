a72bf3bb7208dbac3452a9bb6d93c0db9549a11e
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-05-28 10:50:46 +0200

    x

--- ukesoppgave14/src/components/Player.vue
+++ ukesoppgave14/src/components/Player.vue
@@ -13,12 +13,14 @@ const emit = defineEmits<{
 }>();
 </script>
 <template>
-  <div v-if="!gameStarted" id="Player">
-    <h3>Spillere: {{ players }}</h3>
-    <button :disabled="players >= 4" @click="emit('player', 'increase')">+</button>
-    <button :disabled="players <= 1" @click="emit('player', 'decrease')">-</button>
-    <button @click="emit('startGame')">Start Spill</button>
+  <div>
+    <div v-if="!gameStarted" id="Player">
+      <h3>Spillere: {{ players }}</h3>
+      <button :disabled="players >= 4" @click="emit('player', 'increase')">+</button>
+      <button :disabled="players <= 1" @click="emit('player', 'decrease')">-</button>
+      <button @click="emit('startGame')">Start Spill</button>
+    </div>
+    <button v-if="gameStarted" @click="emit('reStartGame')">Restart Spill</button>
   </div>
-  <button v-if="gameStarted" @click="emit('reStartGame')">Restart Spill</button>
 </template>
 <style scoped></style>
