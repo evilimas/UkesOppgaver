e1f8f7451b2c7ff41cb2225ad2b3f6776caf529d
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-06-01 21:31:25 +0200

    x

--- ukesoppgave14/src/components/Dice.vue
+++ ukesoppgave14/src/components/Dice.vue
@@ -15,33 +15,26 @@ const emit = defineEmits<{
 
 const props = defineProps<Props>();
 
-const trillText = computed(() =>
-  props.throwCount <= 0 ? "Ferdig" : "Ganger igjen"
-);
+const trillText = computed(() => (props.throwCount <= 0 ? "Ferdig" : "Ganger igjen"));
 </script>
 
 <template>
   <fieldset>
     <legend>Spiller: {{ activePlayer }}</legend>
     <div v-show="gameStarted">
-      <button @click="emit('throwDice')" :disabled="throwCount <= 0">
-        Trill terninger
-      </button>
+      <button @click="emit('throwDice')" :disabled="throwCount <= 0">Trill terninger</button>
       <div>{{ throwCount }} {{ trillText }}</div>
 
       <div class="dice" style="display: flex" :disabled="throwCount === 3">
-        <span 
-          v-for="dieData of diceObjects" 
+        <span
+          v-for="dieData of diceObjects"
           :key="dieData.index"
           :style="dieData.style"
           @click="emit('flip', dieData.index)"
-          >
+        >
           <div>
             {{ dieData.char }}
           </div>
-          <!-- <div :style="dieObject.style()" @click="emit('flip', dieObject.index)">
-            {{ dieObject.char }}
-          </div> -->
         </span>
       </div>
     </div>
