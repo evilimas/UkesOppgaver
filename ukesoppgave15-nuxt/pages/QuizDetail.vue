8960e3b17103471de8695246af335cd988312672
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-06-02 11:25:38 +0200

    ukesoppgave15 med nuxt

---
+++ ukesoppgave15-nuxt/pages/QuizDetail.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const props = defineProps<{ quizId: string }>();
+const quiz = computed(() => quizzes.find((q) => q.id === props.quizId));
+
+const currentQuestion = ref(0);
+const answers = ref<number[]>([]);
+const answered = ref(false);
+
+function answerQuestion(selectedIdx: number) {
+  answers.value[currentQuestion.value] = selectedIdx;
+  answered.value = true;
+}
+function nextQuestion() {
+  answered.value = false;
+  currentQuestion.value++;
+}
+
+function restart() {
+  currentQuestion.value = 0;
+  answers.value = [];
+  answered.value = false;
+}
+
+watch(() => props.quizId, restart);
+</script>
+
+<template>
+  <div v-if="quiz">
+    <h1>{{ quiz.title }}</h1>
+    <div v-if="currentQuestion < quiz.questions.length">
+      <QuestionView
+        :question="quiz.questions[currentQuestion]"
+        :answered="answered"
+        :selected="answers[currentQuestion]"
+        @answer="answerQuestion"
+        @next="nextQuestion"
+      />
+    </div>
+    <QuizSummary
+      v-else
+      :questions="quiz.questions"
+      :answers="answers"
+      @restart="restart"
+    />
+  </div>
+  <div v-else>
+    <p>Quiz ikke funnet.</p>
+  </div>
+</template>
---
+++ ukesoppgave15-nuxt/types/quiz.ts
@@ -0,0 +1,13 @@
+interface Quiz {
+  id: string
+  title: string
+  questions: Question[]
+}
+
+interface Question {
+  question: string
+  options: string[]
+  answer: number // index i options-arrayen
+}
+
+export type {Quiz, Question};
