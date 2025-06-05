0251a6a66e11e8cf8df20f47e98df6bb70b6ee77
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-06-05 13:51:18 +0200

    x

--- ukesoppgave15-react/src/components/QuizSummary.tsx
+++ ukesoppgave15-react/src/components/QuizSummary.tsx
@@ -1,5 +1,13 @@
 import type { Question } from '../types/quiz';
-function QuizSummary({ questions, answers, restart }) {
+function QuizSummary({
+  questions,
+  answers,
+  restart,
+}: {
+  questions: any;
+  answers: any;
+  restart: () => void;
+}) {
   const isCorrectAnswer = (answer: string, index: number) =>
     answer === questions[index].answer;
   const correctCount = answers.filter((answer: string, index: number) =>
@@ -33,37 +41,3 @@ function QuizSummary({ questions, answers, restart }) {
 }
 
 export default QuizSummary;
-
-{
-  /* <template>
-    <div>
-        <h2>Resultat</h2>
-        <p>Du fikk {{ correctCount }} av {{ questions.length }} riktige.</p>
-        <ul>
-            <li v-for="(q, idx) in questions" :key="q.question">
-                {{ q.question }}<br />
-                <small>
-                    Ditt svar: {{ q.options[answers[idx]] ?? 'ikke svart' }}<br>
-                    Riktig: {{ q.options[q.answer] }}
-                </small>
-            </li>
-        </ul>
-        <button @click="$emit('restart')">Spill igjen</button>
-        <br />
-        <router-link to="/">Ta en ny quiz</router-link>
-    </div>
-</template>
-
-<script setup lang="ts">
-import { defineProps, computed } from 'vue';
-import type { Question } from '../types/quiz';
-
-const props = defineProps<{
-    questions: Question[];
-    answers: number[]
-}>();
-
-const isCorrectAnswer = (answer: number, index: number) => answer === props.questions[index].answer;
-const correctCount = computed(() => props.answers.filter(isCorrectAnswer).length);
-</script> */
-}
