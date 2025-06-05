1d899a743082b8079add6d56c081450885078344
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-06-05 14:22:34 +0200

    made a quiz app in 20 min with claude and githubcopilot

--- ukesoppgave15-react/src/main.tsx
+++ ukesoppgave15-react/src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import './index.css';
 import App from './App.tsx';
 import QuizDetail from './QuizDetail.tsx';
-import { quizzes } from './data/quizzes.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
