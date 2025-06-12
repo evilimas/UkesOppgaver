d0ace6910413b388af19c1d10109f51d4d68e386
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-06-12 12:56:59 +0200

    x

--- ukesoppgave16/src/app/pages/quiz-details/quiz-details.ts
+++ ukesoppgave16/src/app/pages/quiz-details/quiz-details.ts
@@ -1,5 +1,6 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component, signal, inject } from '@angular/core';
+import { Component, signal, inject, computed } from '@angular/core';
+import { quizzes } from '../../../data/quizes';
 
 @Component({
   selector: 'app-quiz-details',
@@ -8,8 +9,10 @@ import { Component, signal, inject } from '@angular/core';
   styleUrl: './quiz-details.css',
 })
 export class QuizDetails {
+  quizzes = quizzes;
   quizId = signal('');
   private activatedRoute = inject(ActivatedRoute);
+  quiz = computed(() => quizzes.find((q) => q.id === this.quizId));
 
   onstructor() {
     this.activatedRoute.params.subscribe((params) => {
