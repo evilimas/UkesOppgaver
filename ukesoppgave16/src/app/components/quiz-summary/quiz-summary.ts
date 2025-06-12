ba3e99962408c65e4b9bf8c274eb1542e8238228
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-06-12 14:56:23 +0200

    x

--- ukesoppgave16/src/app/components/quiz-summary/quiz-summary.ts
+++ ukesoppgave16/src/app/components/quiz-summary/quiz-summary.ts
@@ -1,13 +1,22 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, computed } from '@angular/core';
 
 @Component({
   selector: 'app-quiz-summary',
   imports: [],
   templateUrl: './quiz-summary.html',
-  styleUrl: './quiz-summary.css'
+  styleUrl: './quiz-summary.css',
 })
 export class QuizSummary {
-@Input() restart = () => {
-  console.log('Restarting quiz...');
-};
+  isCorrectAnswer = (answer: number, index: number) =>
+    answer === this.questions[index].answer;
+
+  correctCount = computed(
+    () => this.answers.filter(this.isCorrectAnswer).length
+  );
+
+  @Input() questions: { answer: number }[] = [];
+  @Input() answers: number[] = [];
+  @Input() restart = () => {
+    console.log('Restarting quiz...');
+  };
 }
