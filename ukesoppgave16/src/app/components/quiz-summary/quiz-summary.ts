0558a37253f4d8cbe11e8fa2197a8193aa44faa9
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-06-13 09:33:14 +0200

    amy fix

--- ukesoppgave16/src/app/components/quiz-summary/quiz-summary.ts
+++ ukesoppgave16/src/app/components/quiz-summary/quiz-summary.ts
@@ -1,22 +1,25 @@
-import { Component, Input, computed } from '@angular/core';
+import { Component, Input, computed, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quiz-summary',
-  imports: [],
   templateUrl: './quiz-summary.html',
   styleUrl: './quiz-summary.css',
+  standalone: true, // if using standalone components
 })
 export class QuizSummary {
-  isCorrectAnswer = (answer: number, index: number) =>
-    answer === this.questions[index].answer;
+  // isCorrectAnswer = (answer: number, index: number) =>
+  //   answer === this.questions[index].answer;
 
-  correctCount = computed(
-    () => this.answers.filter(this.isCorrectAnswer).length
-  );
+  // correctCount = computed(
+  //   () => this.answers.filter(this.isCorrectAnswer).length
+  // );
 
-  @Input() questions: { answer: number }[] = [];
-  @Input() answers: number[] = [];
-  @Input() restart = () => {
-    console.log('Restarting quiz...');
-  };
+  // @Input() questions: { answer: number }[] = [];
+  // @Input() answers: number[] = [];
+  // @Input() restart = () => {
+  //   console.log('Restarting quiz...');
+  // };
+  @Input() questions: any;
+  @Input() answers: any;
+  @Output() restart = new EventEmitter<void>();
 }
