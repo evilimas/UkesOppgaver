58f1684adb71c63097a17d982a1888a1a8d0bbcd
Egidijus Vilimas <eg.vilimas@gmail.com>
2025-06-16 12:54:31 +0200

    x

--- ukesoppgave16/src/app/app.routes.ts
+++ ukesoppgave16/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { QuizDetails } from './pages/quiz-details/quiz-details';
 import { QuizList } from './pages/quiz-list/quiz-list';
-import { Test } from './pages/test/test';
 
 export const routes: Routes = [
   { path: '', component: QuizList },
   { path: 'quiz/:id', component: QuizDetails },
-  { path: 'test', component: Test },
 ];
